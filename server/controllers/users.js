14c56012e2d8cb58d9d23949842caee654bd0e1a
alimirhashimli <alimirhashimli@gmail.com>
2023-10-11 23:57:43 +0200

    server push

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,4 @@
-import User from "../models/user";
-
+import User from "../models/user.js";
 
 // READ
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -11,3 +11,6 @@ router.get("/:userId/posts", verifyToken, getUserPosts);
 
 // update
 router.patch("/:id/like", verifyToken, likePost)
+
+
+export default router;
