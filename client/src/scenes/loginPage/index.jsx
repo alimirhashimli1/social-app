18e4e54321020a1e1cbdb3f4b293f62eb25143fe
alimirhashimli <alimirhashimli@gmail.com>
2023-10-25 02:33:33 +0200

    adding the loginPage

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,22 @@
+import {Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
     return (
-      <div>LoginPage</div>
+      <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+
+       
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"          
+        >
+          Sociopedia
+        </Typography>
+        </Box>
+      </Box>
     )
   }
   
